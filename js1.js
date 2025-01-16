fa8cf84e3f3c5c47d781182d97b0ed0f7b4264cb
nadiagalay <nadiakidstv@gmail.com>
2025-01-16 13:58:50 +0200

    start

--- js1.js
+++ js1.js
@@ -1,7 +1,8 @@
 document.getElementById("MyFristButton").onclick=function(e){
     let a=prompt("Enter a number");
     let b=prompt("Enter another number");
+    console.log(typeof (a));
     let p=a*b;
-    alert("Периметр" +p);
+    alert("Периметр "+p);
 
 }   
