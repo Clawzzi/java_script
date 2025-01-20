343e31c992f490585142e0a9809aac6d3d820dfb
nadiagalay <nadiakidstv@gmail.com>
2025-01-20 09:46:48 +0200

    start

--- js1.js
+++ js1.js
@@ -1,8 +1,21 @@
-document.getElementById("MyFristButton").onclick=function(e){
-    let a=prompt("Enter a number");
-    let b=prompt("Enter another number");
-    console.log(typeof (a));
-    let p=a*b;
-    alert("Периметр "+p);
+document.getElementById("MyFristButton").onclick = function() {
+    let a = prompt("Введіть коефіцієнт a:");
+    let b = prompt("Введіть коефіцієнт b:");
+    let c = prompt("Введіть коефіцієнт c:");
 
-}   
+    a = parseFloat(a);
+    b = parseFloat(b);
+    c = parseFloat(c);
+
+    let discriminant = b * b - 4 * a * c;
+    if (discriminant > 0) {
+        let x1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+        let x2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+        alert("Корені рівняння: x1 = " + x1 + ", x2 = " + x2);
+    } else if (discriminant === 0) {
+        let x = -b / (2 * a);
+        alert("Рівняння має один корінь: x = " + x);
+    } else {
+        alert("Рівняння не має дійсних коренів.");
+    }
+};
